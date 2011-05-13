a78641843408d9aea879f4cdae02fb7e361d0298
unknown <Vince@.(none)>
2011-05-12 17:37:41 -0700

    Project files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Weeve::Application.config.secret_token = 'f3a273a5e1f4b0a99041c503784fca02fec75126eded3bc0ba579cf73388a1d02228960b2ceaa01ca7ff7f6a6850db0b844ffe87c27a2fbc85c41ea2c2d9eece'
